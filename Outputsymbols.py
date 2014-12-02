c33e94eceb16732403b92941efd3f4221fbfaa07
RyanLongRoad <4327@longroad.ac.uk>
2014-12-02 11:11:00 +0000

    (Not finished) output symbols revision exercise

---
+++ Outputsymbols.py
@@ -0,0 +1,17 @@
+#Ryan Cox
+#02/12/14
+#Write and test a function OutputSymbols, which takes two parameters: an integer n and a character symbol. The function is to display, on the same line, the symbol n times.
+
+
+n = int(input("Please enter the number of repeats: "))
+character = input("please eneter the character: ")
+
+
+for loops in range(n):
+    print(character)
+
+def engine():
+
+#Main Program
+
+engine():
